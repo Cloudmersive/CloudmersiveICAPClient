6cc72476cc9d1ab843038dbe6ac4631dd10d98d8
Cloudmersive <support@cloudmersive.com>
2025-02-12 21:52:17 -0800

    Added

--- CloudmersiveIcapClient/CloudmersiveIcapClient/Program.cs
+++ CloudmersiveIcapClient/CloudmersiveIcapClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,14 @@ namespace CloudmersiveIcapClient
     {
         static void Main(string[] args)
         {
+            byte[] download = File.ReadAllBytes(args[0]);
+            CloudmersiveIcapClient icap = new CloudmersiveIcapClient(args[1], 1344, "reqmod", false);
+            icap.Method = "REQMOD";
+
+
+            int result = icap.scanFileWithoutPreview(download);
+
+            Console.WriteLine("Response Code: " + result.ToString());
         }
     }
 }
